8be968e5eb17936c328acc417f376de234e9db1b
Alexandre Mattenet <gadevoi@gmail.com>
2015-04-06 19:28:49 +0200

    Projet créé

---
+++ app/src/androidTest/java/lsin1225/uclouvain/be/bartenders/ApplicationTest.java
@@ -0,0 +1,13 @@
+package lsin1225.uclouvain.be.bartenders;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
